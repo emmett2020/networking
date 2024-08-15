b615383b9de886157b495657dbeed5df8bcef46e
xiaomingZhang2020 <xiaomingZhang2020@outlook.com>
2024-08-15 23:25:21 +0800

    update more copyright

--- tests/test_http1_message_parser.cpp
+++ tests/test_http1_message_parser.cpp
@@ -1,4 +1,5 @@
-/* Copyright 2023 Xiaoming Zhang
+/*
+ * Copyright (c) 2024 Xiaoming Zhang
  *
  * Licensed under the Apache License Version 2.0 with LLVM Exceptions
  * (the "License"); you may not use this file except in compliance with
